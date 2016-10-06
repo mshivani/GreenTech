9149fea946cbbaa56e073a32e067f8ca88e0a475
Shivani Murali <mshivani@udel.edu>
2016-10-05 23:37:33 -0400

    Created add entry view controller

---
+++ GreenTech/AddEntryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddEntryViewController.swift
+//  GreenTech
+//
+//  Created by Shivani Murali on 10/5/16.
+//  Copyright © 2016 Shivani Murali. All rights reserved.
+//
+
+import UIKit
+
+class AddEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
