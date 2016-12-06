06bc4f2d655b24fa0f17aba8a2efbe37c8d8188b
Charles Anders <Brandon_Anders@Charless-MacBook-Pro.local>
2016-12-05 21:49:44 -0500

    Email Pic and Table

--- GreenTech/UploadViewController.swift
+++ GreenTech/UploadViewController.swift
@@ -21,12 +21,11 @@ class UploadViewController: UIViewController, MFMailComposeViewControllerDelegat
         let mailComposeController = MFMailComposeViewController()
         
         mailComposeController.mailComposeDelegate = self;
-        
-        mailComposeController.setToRecipients(["dearley@udel.edu"])
+
         
         mailComposeController.setSubject("GreenTech Information")
         
-        mailComposeController.setMessageBody("This is an email", isHTML: false)
+        mailComposeController.setMessageBody("<div style='background-color: #dad7e1; padding: 5px 5px 5px 65px; width: 685px;'><strong>Research Site Name:</strong><br /><table border='1' cellpadding='5'><tbody><tr style='background-color: #a9abfe;'><th>&nbsp;</th><th>Tons CO2</th><th title='MWh of energy generated per year'>MWh Energy</th><th>Intensity</th></tr><tr style='background-color: #daebeb;'><td>2000</td><td style='text-align: right;'>13,520</td><td style='text-align: right;'>21,824</td><td style='text-align: right;'>620</td></tr><tr style='background-color: #daebeb;'><td>present</td><td style='text-align: right;'>7,938</td><td style='text-align: right;'>12,863</td><td style='text-align: right;'>617</td></tr><tr style='background-color: #daebeb;'><td>future</td><td style='text-align: right;'>7,938</td><td style='text-align: right;'>12,863</td><td style='text-align: right;'>617</td></tr></tbody></table><br /><table border='1'><tbody><tr style='background-color: #a9abfe;'><td style='width: 50px;'><span class='bold'> Power </span></td><td style='width: 150px;'><span class='bold'> Type </span></td><td style='width: 80px;'><span class='bold'> Status </span></td><td style='width: 80px;'><span class='bold'> Fuel </span></td><td style='width: 80px;' colspan='1'><span class='bold'> Secondary Fuel(s) </span></td></tr><tr style='background-color: #daebeb;'><td>45 MW</td><td>Burner: Turbine</td><td>Operating</td><td style='padding-left: 4px; padding-right: 4px;'>Natural Gas</td><td style='padding-left: 4px; padding-right: 4px;'>Oil</td></tr><tr style='background-color: #f2feee;'><td>51 MW</td><td>Burner: Turbine</td><td>Operating</td><td style='padding-left: 4px; padding-right: 4px;'>Natural Gas</td><td style='padding-left: 4px; padding-right: 4px;'>Oil</td></tr></tbody></table></div>", isHTML: true)
         
         
         
